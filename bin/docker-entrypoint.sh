ca904cbe0a659e64144bc73a75f2318994ab4e90
Danny Hiemstra <dannyhiemstra@gmail.com>
2017-09-21 17:31:43 +0200

    Final tweaks

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,13 +6,14 @@ CMD="${1:-web}"
 
 cd $APP_PATH
 
+bundle exec rake db:migrate
+
 case "$CMD" in
   clockwork)
     bundle exec clockwork clock.rb
     exit 0
     ;;
   web)
-    bundle exec rake db:migrate
     bundle exec puma -b tcp://0.0.0.0 -e production -p 80 -w 1 -t 2:16 config.ru
     exit 0
     ;;
